57764dc3d83378a46d0ba85275fdb4422dde5d4b
aabastos <andrebastos1198@gmail.com>
2020-06-17 12:41:54 -0300

    first codes to add notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { v1 as uuid } from "uuid";
 import Heading from "./header/Heading";
+import Create from "./addNote/CreateArea";
 import Footing from "./footer/Footing";
 import Note from "./note/Note";
-import notes from "../notes";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote() {
+    const newNote = {
+      id: new uuid(),
+    };
+
+    console.log(newNote);
+    setNotes((prevNotes) => {
+      return [...prevNotes];
+    });
+  }
+
   return (
     <div>
       <Heading />
+      <Create />
       {notes.map((note) => (
         <Note key={note.key} title={note.title} content={note.content} />
       ))}
